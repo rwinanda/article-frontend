509aa01f01ef2419c62cf1cf034a845396bfd19e
rwinanda <mochammadrwinanda@gmail.com>
2025-08-07 18:25:01 +0700

    feat: Update Navbar using redux - Add Navbar for Bg Scroll for UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,26 @@ import Image from "next/image"
 // import Link from "next/link";
 import { useEffect, useState } from "react";
 import MenuProfile from "./user-profile/MenuProfile";
+import { useAppSelector } from "@/redux/hook";
+import Link from "next/link";
 
 const Navbar = () => {
     const [profileResp, setProfileResp] = useState<ProfileResponse>({} as ProfileResponse)
     const [openMenuProfile, setOpenMenuProfile] = useState(false);
+    const navbarBgScroll = useAppSelector((state) => state.navbar)
 
     useEffect(() => {
         HandlerProfile(setProfileResp);
     }, []);
 
     return (
-        <div>
-            <nav className="flex w-full z-0 justify-between">
-                <div className="my-9 ml-15">
-                    <Image src="/images/logoipsum-white.png" alt="header-bg" width='134' height='24' className="opacity-85" />
-                </div>
+        <div className="z-50 w-full transition-all duration-300">
+            <nav className={`flex fixed w-full z-10 justify-between transition-all duration-100 ${navbarBgScroll.navbar}`}>
+                <Link
+                    href={"/"}
+                    className="my-9 ml-15">
+                    <Image src={navbarBgScroll.logoImage} alt="header-bg" width='134' height='24' className="opacity-85 transition-all duration-100" />
+                </Link>
                 <div className="flex flex-col">
                     <button
                         type="button"
@@ -26,11 +31,11 @@ const Navbar = () => {
                         onClick={() => setOpenMenuProfile(true)}
                     >
                         <div className="w-8 h-8 mr-1.5 flex items-center justify-center bg-blue-200 rounded-full">
-                            <p className="text-xl uppercase font-medium text-blue-900">
+                            <p className={`text-xl uppercase font-medium text-blue-900`}>
                                 {profileResp.username?.[0]}
                             </p>
                         </div>
-                        <p className="font-medium text-white text-[16px] underline">
+                        <p className={`font-medium transition-all duration-100 ${navbarBgScroll.fontColor} text-[16px] underline`}>
                             {profileResp.username}
                         </p>
                     </button>
