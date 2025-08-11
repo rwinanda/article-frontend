c5a9b6ea7f096be317a2a948c65e196b6d4d378f
rwinanda <mochammadrwinanda@gmail.com>
2025-08-11 19:40:31 +0700

    feat: articles admin page
    - Create TableAdmin
    - Calling Debouncesearch

--- src/app/admin/articles/page.tsx
+++ src/app/admin/articles/page.tsx
@@ -4,27 +4,23 @@ import AddNavigate from "@/components/AddNavigate"
 import ContentPages from "@/components/admin-component/ContentPages"
 import PaginationAdmin from "@/components/admin-component/PaginationAdmin"
 import Search from "@/components/admin-component/Search"
+import TableAdmin from "@/components/admin-component/TableAdmin";
 import TableNotFound from "@/components/admin-component/TableNotFound";
 import TitlePages from "@/components/admin-component/TitlePages"
 import TopContent from "@/components/admin-component/TopContent"
 import SkeletonArticle from "@/components/loading/loading-skeleton/SkeletonArticle";
-import AlertModal from "@/components/popup/AlertModal";
 import NotifAlert from "@/components/popup/notif/NotifAlert";
 import SelectCategory from "@/components/select/SelectCategory";
-import { HandlerDateFormat } from "@/hooks/HandlerDateFormat";
+import { DebounceSearch } from "@/hooks/DebounceSearch";
 import { DeleteArticleAPI, GetAllArticleAPI } from "@/services/articleService";
 import { GetAllCategoryAPI } from "@/services/categoryService";
 import { ArticleQuery, ArticleResponse } from "@/types/articleTypes";
 import { CategoryParam, CategoryResponse } from "@/types/categoryTypes";
-import Image from "next/image";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const ArticlePage = () => {
-    const labelHeaders = ["Thumbnails", "Title", "Category", "Created At", "Action"];
     const [openModalDel, setOpenModalDel] = useState(false);
     const [articleResp, setArticleResp] = useState<ArticleResponse>({} as ArticleResponse);
-    const [selectedData, setSelectedData] = useState("");
     const [categoryResp, setCategoryResp] = useState<CategoryResponse>({} as CategoryResponse);
     const [searchTerm, setSearchTerm] = useState("");
     const [articleParam, setArticleParam] = useState<ArticleQuery>({
@@ -60,18 +56,8 @@ const ArticlePage = () => {
         setArticleParam({ ...articleParam, page })
     };
 
-    // Debounce for search
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setArticleParam((prev) => ({
-                ...prev,
-                title: searchTerm,
-                page: 1,
-            }));
-        }, 500); // 800ms debounce
-
-        return () => clearTimeout(handler)
-    }, [searchTerm]);
+    // Searching
+    DebounceSearch(searchTerm, setArticleParam);
 
     useEffect(() => {
         const categoryParam = {
@@ -114,7 +100,6 @@ const ArticlePage = () => {
             <ContentPages >
                 <TopContent text={`Total Article: ${articleResp.total}`} />
 
-                {/* Filter */}
                 <div className="flex p-6 border-b-1 border-slate-200">
                     <div className="flex">
                         {/* Category */}
@@ -133,74 +118,7 @@ const ArticlePage = () => {
                 ) : articleResp.data?.length === 0 ? (
                     <TableNotFound />
                 ) :
-                    <div className="flex flex-col">
-                        {/* Header Table */}
-                        <div className="grid grid-cols-5 w-full items-center text-center bg-gray-100 py-3 transition duration-300 ease-in-out">
-                            {labelHeaders.map((label, index) => (
-                                <label key={index}>
-                                    {label}
-                                </label>
-                            ))}
-                        </div>
-
-                        {/* Data Table */}
-                        {articleResp.data?.map((article, index) => {
-                            const rawDate = article?.updatedAt;
-                            const newDate = HandlerDateFormat(rawDate);
-                            return (
-                                <div
-                                    key={index}
-                                    className="grid grid-cols-5 w-full items-center py-3 border-1 border-slate-200 transition duration-300 ease-in-out hover:bg-gray-100">
-                                    <div className="flex justify-center items-center">
-                                        <div className="w-16 h-16 relative">
-                                            {
-                                                !article.imageUrl ?
-                                                    <Image
-                                                        src="/images/image-not-available.jpg"
-                                                        alt="image-not-available"
-                                                        fill
-                                                        className='object-cover border-gray-400 rounded-xl'
-                                                    /> :
-                                                    <Image
-                                                        src={article.imageUrl}
-                                                        alt={`${article.title}-thumbnails`}
-                                                        fill
-                                                        sizes="64px"
-                                                        className="object-cover rounded-md"
-                                                        style={{ objectFit: "cover" }}
-                                                    />
-                                            }
-                                        </div>
-                                    </div>
-                                    <label className="mx-4 my-3 line-clamp-2">
-                                        {article?.title}
-                                    </label>
-                                    <label className="text-center">
-                                        {article?.category?.name}
-                                    </label>
-                                    <label className="text-center">
-                                        {newDate}
-                                    </label>
-                                    <div className="flex justify-center">
-                                        <a href={`/article/${article?.id}`} className="text-blue-600 underline hover:scale-110">Preview</a>
-                                        <Link href={`/admin/articles/update/${article?.id}`} className="mx-3 text-blue-600 underline hover:scale-110">
-                                            Edit
-                                        </Link>
-                                        <button
-                                            className="text-red-500 underline hover:scale-110"
-                                            onClick={() => {
-                                                setSelectedData(article.id)
-                                                setOpenModalDel(true)
-                                            }}
-                                        >
-                                            Delete
-                                        </button>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                        <AlertModal nameModal="Delete Articles" textModal="Deleting this article is permanent and cannot be undone. All related content will be removed." nameButton="Delete" openModal={openModalDel} closeModal={() => setOpenModalDel(false)} handler={() => deleteArticle(selectedData)} bgColor="bg-red-600 hover:bg-red-700" />
-                    </div>
+                    <TableAdmin articleResp={articleResp} deleteArticle={deleteArticle} openModalDel={openModalDel} setOpenModalDel={setOpenModalDel} />
                 }
 
                 <PaginationAdmin currentPage={articleResp.page} totalData={articleResp.total} limitData={articleResp.limit} onPageChange={onPageArticleChange} />
--- src/components/admin-component/TableAdmin.tsx
+++ src/components/admin-component/TableAdmin.tsx
@@ -1,46 +1,90 @@
+import { ArticleResponse } from "@/types/articleTypes"
+import AlertModal from "../popup/AlertModal"
+import { HandlerDateFormat } from "@/hooks/HandlerDateFormat"
+import Image from "next/image"
+import Link from "next/link"
+import { Dispatch, SetStateAction, useState } from "react"
+
 interface props {
-    label: string[]
-    classHeader: string | undefined
-    classData: string | undefined
+    articleResp: ArticleResponse
+    deleteArticle: (id: string) => void
+    openModalDel: boolean
+    setOpenModalDel: Dispatch<SetStateAction<boolean>>
 }
 
-const TableAdmin = ({ label, classHeader, classData }: props) => {
+const TableAdmin = ({ articleResp, deleteArticle, openModalDel, setOpenModalDel }: props) => {
+    const [selectedData, setSelectedData] = useState("");
+    const labelHeaders = ["Thumbnails", "Title", "Category", "Created At", "Action"];
+
+
     return (
-        <div className="">
+        <div className="flex flex-col">
             {/* Header Table */}
-            <div className=
-                {classHeader}
-            >
-                {/* Title Table */}
-                {label.map((dataLabel, index) => (
+            <div className="grid grid-cols-5 w-full items-center text-center bg-gray-100 py-3 transition duration-300 ease-in-out">
+                {labelHeaders.map((label, index) => (
                     <label key={index}>
-                        {dataLabel}
+                        {label}
                     </label>
                 ))}
             </div>
 
             {/* Data Table */}
-            <div className=
-                {classData}
-            >
-                <label className="text-center">
-                    Thumbnails
-                </label>
-                <label className="flex mx-4 my-3">
-                    Figma New Dev Mode: A Game-Changer for Designers & Developers
-                </label>
-                <label className="text-center">
-                    Category
-                </label>
-                <label className="text-center">
-                    Created At
-                </label>
-                <label className="text-center">
-                    <a href="" className="text-blue-600 underline">Preview</a>
-                    <a href="" className="mx-3 text-blue-600 underline">Edit</a>
-                    <button className="text-red-500 underline">Delete</button>
-                </label>
-            </div>
+            {articleResp.data?.map((article, index) => {
+                const rawDate = article?.updatedAt;
+                const newDate = HandlerDateFormat(rawDate);
+                return (
+                    <div
+                        key={index}
+                        className="grid grid-cols-5 w-full items-center py-3 border-1 border-slate-200 transition duration-300 ease-in-out hover:bg-gray-100">
+                        <div className="flex justify-center items-center">
+                            <div className="w-16 h-16 relative">
+                                {
+                                    !article.imageUrl ?
+                                        <Image
+                                            src="/images/image-not-available.jpg"
+                                            alt="image-not-available"
+                                            fill
+                                            className='object-cover border-gray-400 rounded-xl'
+                                        /> :
+                                        <Image
+                                            src={article.imageUrl}
+                                            alt={`${article.title}-thumbnails`}
+                                            fill
+                                            sizes="64px"
+                                            className="object-cover rounded-md"
+                                            style={{ objectFit: "cover" }}
+                                        />
+                                }
+                            </div>
+                        </div>
+                        <label className="mx-4 my-3 line-clamp-2">
+                            {article?.title}
+                        </label>
+                        <label className="text-center">
+                            {article?.category?.name}
+                        </label>
+                        <label className="text-center">
+                            {newDate}
+                        </label>
+                        <div className="flex justify-center">
+                            <a href={`/article/${article?.id}`} className="text-blue-600 underline hover:scale-110">Preview</a>
+                            <Link href={`/admin/articles/update/${article?.id}`} className="mx-3 text-blue-600 underline hover:scale-110">
+                                Edit
+                            </Link>
+                            <button
+                                className="text-red-500 underline hover:scale-110"
+                                onClick={() => {
+                                    setSelectedData(article.id)
+                                    setOpenModalDel(true)
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                )
+            })}
+            <AlertModal nameModal="Delete Articles" textModal="Deleting this article is permanent and cannot be undone. All related content will be removed." nameButton="Delete" openModal={openModalDel} closeModal={() => setOpenModalDel(false)} handler={() => deleteArticle(selectedData)} bgColor="bg-red-600 hover:bg-red-700" />
         </div>
     )
 }
