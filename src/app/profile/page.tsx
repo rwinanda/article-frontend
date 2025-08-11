b350ec2d7a462175faeeeb345f50c847e914709a
rwinanda <mochammadrwinanda@gmail.com>
2025-08-11 19:50:55 +0700

    feat: profile
    - add navbar

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
-import NavbarSecond from "@/components/NavbarSecond";
+import Navbar from "@/components/Navbar";
 import ProfileUserComponent from "@/components/ProfileUserComponent";
+import { setNavbarBgScroll } from "@/redux/features/navbar/navbarSlice";
+import { useAppDispatch } from "@/redux/hook";
+import { useEffect } from "react";
 
 const ProfileUser = () => {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(setNavbarBgScroll(({
+            navbar: "bg-white shadow-sm",
+            fontColor: "text-black",
+            logoImage: "/images/logoipsum.png"
+        })));
+    }, [dispatch]);
+
     return (
         <div className="flex flex-col">
-            <div className='relative w-full'>
-                <NavbarSecond />
-            </div>
+            <Navbar />
             <ProfileUserComponent textBtn="Back to Home" href="/" marginTop="mt-28" />
         </div>
     )
