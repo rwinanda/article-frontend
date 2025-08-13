07c0a11ddd7a7d7817b4230997ac600a57b1f1fd
rwinanda <mochammadrwinanda@gmail.com>
2025-08-13 14:11:40 +0700

    feat: Button Component - add props for onClick

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,20 @@
+import { MouseEventHandler } from "react"
 import ButtonLoading from "./loading/ButtonLoading"
 
 interface ButtonProps {
     name: string
     className: string | undefined
     type: "button" | "submit" | "reset"
-    loading: boolean
+    loading?: boolean
+    onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-const ButtonComponent = ({ name, className, type, loading }: ButtonProps) => {
+const ButtonComponent = ({ name, className, type, loading, onClick }: ButtonProps) => {
     return (
         <button
             type={type}
             className={className}
+            onClick={onClick}
         >
             {loading ? <ButtonLoading/> : name }
         </button>
