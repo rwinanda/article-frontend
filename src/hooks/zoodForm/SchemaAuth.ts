4561bc0265d5e79178e60f7dc7d0ea1b188a093a
rwinanda <mochammadrwinanda@gmail.com>
2025-08-16 23:48:33 +0700

    feat: schema auth - Schema for login / register for Zod Form

--- src/hooks/zoodForm/SchemaAuth.ts
+++ src/hooks/zoodForm/SchemaAuth.ts
@@ -3,6 +3,7 @@ import z from "zod";
 export const SchemaRegist = z.object({
     username: z.string().min(2, 'Username field cannot be empty'),
     password: z.string().min(8, 'Password must be at least 8 characters long'),
+    role: z.string().min(1, 'Role cannot be empty')
 });
 
 export const SchemaLogin = z.object({
