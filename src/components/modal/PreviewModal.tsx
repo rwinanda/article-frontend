1a2b8c29061f2a508ce3f98eeb7bbfc3deeb45e3
rwinanda <mochammadrwinanda@gmail.com>
2025-08-29 17:19:43 +0700

    feat: PreviewModal component
    - add overflow-y-scroll
    - change relative with a flex
    - Delete fill and change to width and height

--- src/components/modal/PreviewModal.tsx
+++ src/components/modal/PreviewModal.tsx
@@ -11,25 +11,27 @@ interface PreviewModalProps {
 
 const PreviewModal = ({ open, closeModal, data, category }: PreviewModalProps) => {
     if (!data) return null
+    
     return (
         <div
             className={`fixed inset-0 flex items-center justify-center z-50 ${open ? "visible bg-black/40" : "invisible"}`}
             onClick={closeModal}
         >
             <div
-                className="bg-white rounded-lg p-6 flex flex-col shadow-xl max-w-2xl w-full"
+                className="bg-white rounded-lg p-6 flex flex-col shadow-xl max-w-2xl w-full max-h-120 overflow-y-scroll"
                 onClick={(e) => {
                     e.stopPropagation()
                 }}
             >
                 <h2 className="text-xl text-center font-bold mb-4">Preview Article</h2>
                 {data.imageUrl && (
-                    <div className="relative w-full h-[240px] rounded-md mb-4 border-1 border-gray-400">
+                    <div className="flex justify-center items-center rounded-md mb-4">
                         <Image
                             src={data.imageUrl}
                             alt={data.title}
-                            fill
-                            className="object-cover rounded border-gray-400"
+                            width={200}
+                            height={200}
+                            className="object-cover rounded border-1 border-gray-400"
                         />
                     </div>
                 )}
